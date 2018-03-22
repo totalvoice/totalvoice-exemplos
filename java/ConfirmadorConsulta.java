6df49bd57ec470158fdf5d19710f7f00f896b217
Guilherme Nunes <guilherme@totalvoice.com.br>
2018-03-22 15:05:00 -0300

    Indentação unificada

--- java/ConfirmadorConsulta.java
+++ java/ConfirmadorConsulta.java
@@ -3,29 +3,27 @@ import br.com.totalvoice.api.Composto;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class ConfirmadorConsulta
-{
+public class ConfirmadorConsulta {
     private static final String MEU_TOKEN = "SEU_TOKEN";
     private TotalVoiceClient cliente = new TotalVoiceClient(MEU_TOKEN);
 
-    public int confirmarConsulta(String numero, String nome, String data) throws Exception
-    {
-		// Cria um Composto da TotalVoice
+    public int confirmarConsulta(String numero, String nome, String data) throws Exception {
+        // Cria um Composto da TotalVoice
         Composto composto = new Composto(cliente);
-		
-		// Criamos um JSON para carregar esse composto
+
+        // Criamos um JSON para carregar esse composto
         JSONObject jsonComposto = new JSONObject();
 
-		// Número para qual o composto será enviado
+        // Número para qual o composto será enviado
         jsonComposto.put("numero_destino", numero);
-		
-		// Se quisermos gravar a ligação
+
+        // Se quisermos gravar a ligação
         jsonComposto.put("gravar_audio", false);
-		
+
         // Pode colocar tags aqui para utilizar depois
         jsonComposto.put("tags", "TAG_PERSONALIZADA");
 
-		// Criamos a array de passos do nosso composto
+        // Criamos a array de passos do nosso composto
         JSONArray dados = new JSONArray();
 
         // --------------
@@ -72,7 +70,7 @@ public class ConfirmadorConsulta
 
         dados.put(ttsCancelou);
 
-		// Coloca todos os passos no composto
+        // Coloca todos os passos no composto
         jsonComposto.put("dados", dados);
 
         // --------------
@@ -81,8 +79,7 @@ public class ConfirmadorConsulta
         JSONObject result = composto.enviar(jsonComposto);
         System.out.println(result);
 
-        if (result.getBoolean("sucesso"))
-        {
+        if (result.getBoolean("sucesso")) {
             System.out.println("Composto enviado com sucesso");
             return result.getJSONObject("dados").getInt("id");
         }
@@ -90,62 +87,53 @@ public class ConfirmadorConsulta
         return -1;
     }
 
-	// Método de exemplo para recuperar os dados de um composto
-    public int conferirConfirmação(int id) throws Exception
-    {
+    // Método de exemplo para recuperar os dados de um composto
+    public int conferirConfirmação(int id) throws Exception {
         Composto composto = new Composto(cliente);
         JSONObject compostoJson = composto.buscar(id);
-		
-		// Print do resultado
-		System.out.println(compostoJson);
-		
-		// Se deu sucesso
-        if (compostoJson.getBoolean("sucesso"))
-        {
-			// Pega as informações do Composto
+
+        // Print do resultado
+        System.out.println(compostoJson);
+
+        // Se deu sucesso
+        if (compostoJson.getBoolean("sucesso")) {
+            // Pega as informações do Composto
             JSONObject dados = compostoJson.getJSONObject("dados");
-			
-			// Resposta null se não respondeu
-            if (dados.isNull("resposta"))
-            {
+
+            // Resposta null se não respondeu
+            if (dados.isNull("resposta")) {
                 return -1;
             }
 
-			// Retorna a resposta
+            // Retorna a resposta
             return dados.getInt("resposta");
         }
 
         return -1;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         try {
             System.out.println("Iniciando confirmador de consultas");
 
-			// Cria o objeto principal
+            // Cria o objeto principal
             ConfirmadorConsulta confirmador = new ConfirmadorConsulta();
 
-			// Chama função que envia um composto
+            // Chama função que envia um composto
             int idConfirmacao = confirmador.confirmarConsulta("484848484848", "Guilherme", "30 de Março");
 
-			// Verifica se o composto foi enviado com sucesso
-            if (idConfirmacao > 0)
-            {
+            // Verifica se o composto foi enviado com sucesso
+            if (idConfirmacao > 0) {
                 // Espera 30 segundos e confere o número digitado pelo cliente
                 Thread.sleep(30 * 1000);
 
-				// Pega a resposta digitada
+                // Pega a resposta digitada
                 int resposta = confirmador.conferirConfirmação(idConfirmacao);
                 System.out.println("Resposta: " + resposta);
-            }
-            else
-            {
+            } else {
                 throw new Exception("Erro ao enviar confirmação");
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.err.println(e);
         }
     }
